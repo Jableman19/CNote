3fdc583a7ba48a905b96d9df44384a758105f1c5
Piyawatchara Mahattanadul <piyawatm@umich.edu>
2023-04-07 18:14:28 -0400

    Play/Pause Button still not working

---
+++ Assets/Scripts/MIDI_Play_Pause.cs
@@ -0,0 +1,58 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class MIDI_Play_Pause : MonoBehaviour
+{
+
+    //// Start is called before the first frame update
+    //void Start()
+    //{
+    //    Button midi_play_pause = transform.Find("UI").Find("MidiPlayPause").GetComponent<Button>();
+    //    TextMeshProUGUI play_pause_text = midi_play_pause.transform.Find("TextPlayPause").GetComponent<TextMeshProUGUI>();
+    //    play_pause_text.text = "play".ToString();
+    //    midi_play_pause.onClick.AddListener(OnPressedMidiPlayPause);
+    //}
+
+    //public void OnPressedMidiPlayPause()
+    //{
+    //    Button midi_play_pause = transform.Find("UI").Find("MidiPlayPause").GetComponent<Button>();
+    //    TextMeshProUGUI play_pause_text = midi_play_pause.transform.Find("TextPlayPause").GetComponent<TextMeshProUGUI>();
+    //    if (MIDIManager.Instance != null)
+    //    {
+    //        if (MIDIManager.Instance.isPlaying())
+    //        {
+    //            MIDIManager.Instance.PauseSong();
+    //            play_pause_text.text = "Play".ToString();
+    //        }
+    //        else
+    //        {
+    //            MIDIManager.Instance.ResumeSong();
+    //            play_pause_text.text = "Pause".ToString();
+    //        }
+    //    }
+    //}
+    SynthManagerFMOD synthManager;
+
+    void Awake()
+    {
+        synthManager = gameObject.GetComponent<SynthManagerFMOD>();
+    }
+    public void PlayPause()
+    {
+            if (MIDIManager.Instance.isPlaying())
+            {
+                MIDIManager.Instance.PauseSong();
+            }
+            else
+            {
+                MIDIManager.Instance.ResumeSong();
+            }
+    }
+
+
+
+}
