6f7dd1a581bbe7a308a74cd2c82d70a03849a709
Jasmine Li <jasmineli106@gmail.com>
2023-04-09 19:16:18 -0400

    ui pause play button has images now

--- Assets/Scripts/MIDI_Play_Pause.cs
+++ Assets/Scripts/MIDI_Play_Pause.cs
@@ -36,25 +36,33 @@ public class MIDI_Play_Pause : MonoBehaviour
     //    }
     //}
     SynthManagerFMOD synthManager;
-    bool playing;
+    [HideInInspector] public bool playing;
+    [HideInInspector] public static MIDI_Play_Pause instance;
+    public Sprite pause;
+    public Sprite play;
+
+    public Image img;
 
     void Awake()
     {
         synthManager = GameObject.FindGameObjectWithTag("SynthManager").GetComponent<SynthManagerFMOD>();
         playing = true;
+        instance = this;
     }
 
     public void PlayPause()
     {
-        if (playing) { synthManager.pause(); }
-        else { synthManager.resume(); }
-        if (MIDIManager.Instance.isPlaying())
+        if (MIDIManager.Instance.isPlaying() || playing)
         {
             MIDIManager.Instance.PauseSong();
+            synthManager.pause(); 
+            img.sprite = play;
         }
         else
         {
             MIDIManager.Instance.ResumeSong();
+            synthManager.resume(); 
+            img.sprite = pause;
         }
 
         playing = !playing;
