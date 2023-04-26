4b1d3b99fc7581ac5f930efe0a5ae38fa4c5714f
Jonah Ableman <70289916+Jableman19@users.noreply.github.com>
2023-04-26 14:15:13 -0400

    fixed ui scaling

--- Assets/Scripts/PopupDynamicRescaler.cs
+++ Assets/Scripts/PopupDynamicRescaler.cs
@@ -4,25 +4,29 @@ using UnityEngine;
 
 public class PopupDynamicRescaler : MonoBehaviour
 {
-    /*public float height = 6;
-    public float width = 9;*/
     public float scale = 1;
+
+    float parentScale;
+    RectTransform rt;
+    Transform parent;
+
+    void Start()
+    {
+        rt = gameObject.GetComponent<RectTransform>();
+        parent = gameObject.transform.parent;
+        parentScale = parent.localScale.x;
+    }
 
     // Update is called once per frame
     void Update()
     {
         //Calculate height, width based on distance
         Vector3 origin = Camera.main.transform.position;
-        Vector3 position = this.gameObject.transform.position;
+        Vector3 position = parent.position;
         float distance = Vector3.Distance(origin, position);
-        /*float newHeight = height * distance*scale;
-        float newWidth = width * distance*scale;*/
 
         //Rescale
-        RectTransform rt = gameObject.GetComponent<RectTransform>();
-        rt.localScale = Vector3.one * distance*scale;
-        /*gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Vertical, newHeight);
-        gameObject.GetComponent<RectTransform>().SetSizeWithCurrentAnchors(RectTransform.Axis.Horizontal, newWidth);*/
+        rt.localScale = Vector3.one*distance*scale * (1/parent.localScale.x);
         
     }
 }
